60d5da8561f93e61503c0b4e60e87a8980e62146
kifuan <kifuan@foxmail.com>
2023-01-05 16:46:28 +0800

    docs: document functions

--- otto.py
+++ otto.py
@@ -38,11 +38,21 @@ SAMPLE_RATE: int = CONFIG['sampleRate']
 PORT: int = CONFIG['port']
 
 
-def make_text_fragments(text: str) -> list[str]:
+def break_text(text: str) -> list[str]:
+    """
+    Breaks text into fragments.
+    :param text: the text to break.
+    :return: text fragments.
+    """
     return [t for t in re.split(f'({"|".join(SPECIAL.keys())})', text) if t]
 
 
 def load_audio_file(filename: Union[str, Iterable[str]]) -> AudioSegment:
+    """
+    Loads audio file or a list of audio files.
+    :param filename: file name or a list of file names.
+    :return: the audio segment.
+    """
     if isinstance(filename, str):
         return normalize(
             AudioSegment.from_file(ASSETS_DIR / filename)
@@ -53,21 +63,36 @@ def load_audio_file(filename: Union[str, Iterable[str]]) -> AudioSegment:
     return reduce(lambda a, b: a + b, (load_audio_file(file) for file in filename))
 
 
-def load_pinyin(pinyin: str) -> AudioSegment:
+def load_pinyin_audio(pinyin: str) -> AudioSegment:
+    """
+    Loads pinyin. It will return a silent segment if not existing.
+    :param pinyin: the pinyin to load.
+    :return: the audio segment.
+    """
     if pinyin not in PINYIN:
         return AudioSegment.silent(MISSING_SILENT_DURATION, SAMPLE_RATE)
     return load_audio_file(PINYIN[pinyin])
 
 
-def load_audio_fragment(fragment: str) -> AudioSegment:
+def load_fragment_audio(fragment: str) -> AudioSegment:
+    """
+    Loads audio for a fragment broken by `break_text`.
+    :param fragment: the fragment to load.
+    :return: the audio segment.
+    """
     if fragment in SPECIAL:
         return load_audio_file(SPECIAL[fragment])
-    return reduce(lambda a, b: a + b, (load_pinyin(pinyin) for pinyin in lazy_pinyin(fragment)))
+    return reduce(lambda a, b: a + b, (load_pinyin_audio(pinyin) for pinyin in lazy_pinyin(fragment)))
 
 
 def make_audio(text: str) -> AudioSegment:
-    fragments = make_text_fragments(text)
-    audio = reduce(lambda a, b: a + b, (load_audio_fragment(frag) for frag in fragments))
+    """
+    Makes an audio for any text.
+    :param text: the text to make an audio.
+    :return: the audio segment.
+    """
+    fragments = break_text(text)
+    audio = reduce(lambda a, b: a + b, (load_fragment_audio(frag) for frag in fragments))
     return audio
 
 
